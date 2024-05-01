cd519c04a3b57a0fab6eef099e038c5365234e43
Mohammad Tahir Khan <mtkhan1@sheffield.ac.uk>
2024-05-01 20:36:22 +0100

    minor changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,4 +10,4 @@ User.create(full_name: 'admin', email: "admin@gmail.com", password: "admin1", pa
 User.create(full_name: 'user', email: "user@gmail.com", password: "usertest1", password_confirmation: "usertest1", user_role: '0', remember_created_at: Time.now)
 User.create(full_name: 'poster', email: "poster@gmail.com", password: "poster1", password_confirmation: "poster1", user_role: '1', remember_created_at: Time.now)
 User.create(full_name: 'poster2', email: "poster2@gmail.com", password: "poster2", password_confirmation: "poster2", user_role: '1', remember_created_at: Time.now)
-User.create(full_name: 'user2', email: "user2@gmail.com", password: "usertest2", password_confirmation: "usertest2", user_role: '0', remember_created_at: Time.now, tags: ["tag1", "tag2"])
+User.create(full_name: 'user2', email: "user2@gmail.com", password: "usertest2", password_confirmation: "usertest2", user_role: '0', remember_created_at: Time.now)
